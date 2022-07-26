ab4bdf4186ca830e85b0d7de4ff0817a7ff59270
FilipPauco <Filip.pauco08@gmail.com>
2022-07-26 20:37:57 +0200

    CourseWork

---
+++ Reminder_Angular/src/app/mock-tasks.ts
@@ -0,0 +1,21 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Doctors Appointment",
+        day: "5th May",
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Food Shopping",
+        day: "6th May",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Work meeting",
+        day: "7th May",
+        reminder: false
+    }
+]
